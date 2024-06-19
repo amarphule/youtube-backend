1be83c44582c392e2ad77840f7dce9ed2bdd3e54
Amar Phule <amardeepphule@gmail.com>
2024-06-19 20:43:33 +0530

    feat: common middleware

--- src/index.js
+++ src/index.js
@@ -5,4 +5,12 @@ dotenv.config({
   path: "./.env",
 });
 
-connectionDB();
+const PORT = process.env.PORT;
+
+connectionDB()
+  .then(() => {
+    console.log(`Server is running on port: ${PORT}`);
+  })
+  .catch((error) => {
+    console.log(`Failed o connect port: ${port},Error: ${error}`);
+  });
