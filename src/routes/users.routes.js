3be2e91023d116138e09d50ff6c748067a463e6f
Amar Phule <amardeepphule@gmail.com>
2024-07-03 00:25:06 +0530

    feat: add endpoints for  user controllers

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
+  refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImaage,
 } from "../controllers/users.controllers.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -29,5 +37,19 @@ router.route("/login").post(loginUser);
 
 // Secured Routes
 router.route("/logout").post(verifyJwt, logoutUser);
+router.route("/refreshToken").post(verifyJwt, refreshAccessToken);
+router.route("/changePasswod").post(verifyJwt, changeCurrentPassword);
+
+router.route("/updateDetails").patch(verifyJwt, updateAccountDetails);
+router
+  .route("/updateAvatar")
+  .patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/updateCoverImage")
+  .patch(verifyJwt, upload.single("coverImage"), updateUserCoverImaage);
+
+router.route("/currentUser").get(verifyJwt, getCurrentUser);
+router.route("/channel/:username").get(verifyJwt, getUserChannelProfile);
+router.route("/history").get(verifyJwt, getWatchHistory);
 
 export default router;
